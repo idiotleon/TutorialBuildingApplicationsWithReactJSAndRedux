ef151f46b061aa7c9792e2247196cef16c73e1c8
Yang Lu <leon@Yangs-MacBook-Pro.local>
2018-03-19 19:24:57 -0700

    demo example: Reducers
    Module 9.4

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,18 @@
+/**
+ * Each reducer handles a specific slice of state
+ * @param {*} state 
+ * @param {*} action 
+ */
+export default function courseReducer(state = [], action){
+    switch(action.type){
+        case 'CREATE_COURSE':
+            // representing the existing array. It ends up returning a new state array
+            return [...state,
+                // a deep copy of the course passed in
+                Object.assign({}, action.course)
+            ];
+        
+        default:
+            return state;
+    }
+}
