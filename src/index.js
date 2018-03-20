c44cbe2ee3389093331740e0c3def62ca96c3df8
Yang Lu <leon@Yangs-MacBook-Pro.local>
2018-03-19 19:55:16 -0700

    demo example: Instantiate Store and Provider
    Module 9.7

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+/*
+*   Provider: higher order component attaching the store
+*   to the react container component
+*/
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';    // Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore();
+
 render(
-    <Router history={browserHistory} routes={routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
     document.getElementById('app')
 );
