dfb2f8a03d90c3a3b967a0888a30e8e96307a2ac
Yang Lu <leon@Yangs-MacBook-Pro.local>
2018-03-19 19:08:07 -0700

    demo example: Binding in ES6
    Module 9.2

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -5,14 +5,26 @@ class CoursesPage extends React.Component{
         super(props, context);
 
         this.state = {
-            course: {title: null}
+            course: {title: ""}
         };
+
+        /*
+        *  To improve performance, one should avoid defining new functions in the renderer function.
+        *  Instead, new functions should be placed in the contructor 
+        */
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
     }
 
     onTitleChange(event){
+        // this needs to be bond with the instance of the component
         const course = this.state.course;
         course.title = event.target.value;
         this.setState({course: course});
+    }
+
+    onClickSave(){
+        alert(`Saving ${this.state.course.title}`);
     }
 
     render(){
