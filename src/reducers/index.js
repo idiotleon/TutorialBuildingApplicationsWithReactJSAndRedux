3134c6f26d2f9fbf9096e51537232e64487ab115
Yang Lu <leon@Yangs-MacBook-Pro.local>
2018-03-19 19:33:42 -0700

    demo example: Root Reducer
    Module 9.5

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+import courses from './courseReducer';
+
+const rootReducer = combineReducers({
+    // ES6 shorthand property name
+    courses
+});
+
+export default rootReducer;
