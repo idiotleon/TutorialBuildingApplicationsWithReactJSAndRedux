f2eb53bdc46feff447137bdfb7cb3dc6ce347b7d
Yang Lu <leon@Yangs-MacBook-Pro.local>
2018-03-20 21:39:17 -0700

    demo example: Remove Inline Form
    Module 10.5

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -6,28 +6,6 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends React.Component{
     constructor(props, context){
         super(props, context);
-
-        this.state = {
-            course: {title: ""}
-        };
-
-        /*
-        *  To improve performance, one should avoid defining new functions in the renderer function.
-        *  Instead, new functions should be placed in the contructor 
-        */
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-
-    onTitleChange(event){
-        // this needs to be bond with the instance of the component
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({course: course});
-    }
-
-    onClickSave(){
-        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index){
@@ -35,14 +13,10 @@ class CoursesPage extends React.Component{
     }
 
     render(){
-        // debugger;
         return(
             <div>
                 <h1>Courses</h1>
                 {this.props.courses.map(this.courseRow)}
-                <h2>Add Course</h2>
-                <input type="text" onChange={this.onTitleChange} value={this.state.course.title}/>
-                <input type="submit" value="Save" onClick={this.onClickSave}/>
             </div>
         );
     }
@@ -54,7 +28,6 @@ CoursesPage.propTypes = {
 };
 
 function mapStateToProps(state, ownProps){
-    // debugger;
     return{
         courses: state.courses
     };
@@ -62,22 +35,8 @@ function mapStateToProps(state, ownProps){
 
 function mapDispatchToProps(dispatch){
     return {
-        // "actions" means mapping to all actions
         actions: bindActionCreators(courseActions, dispatch)
     };
 }
 
-/*
-*   mapDispatchToProps is a function that allows one to fire off his actions
-*   which is optional.
-*   If it is omitted here in the function parameter,
-*   connect[function] will inject a dispatch function for us
-*
-*   Once mapDispatchToProps has been defined, connect will no longer add a dispatch
-*   property on the components: line 51 got ignored
-*/
 export default connect(mapStateToProps, mapDispatchToProps) (CoursesPage);
-/*  equivalently:
-*   const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
-*   export default connectedStateAndProps(CoursePage);
-*/
