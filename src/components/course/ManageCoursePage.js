9c6ef692b0d0797ad51fc7928c7347ec4c421da1
Leon Lu <yanglyu.leon.7@gmail.com>
2018-04-19 10:46:50 -0700

    demo: Update State via componentWillReceiveProps
    Module 11.14

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -29,6 +29,13 @@ class ManageCoursePage extends React.Component {
         this.saveCourse = this.saveCourse.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if (this.props.course.id != nextProps.course.id) {
+            // Necessary to populate form when existing course is loaded directly
+            this.setState({ course: Object.assign({}, nextProps.course) });
+        }
+    }
+
     updateCourseState(event) {
         const field = event.target.name;
         // To avoid mutating state
