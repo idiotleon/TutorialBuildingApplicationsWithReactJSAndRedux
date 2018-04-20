57721d1db5b29aba104ba304e7309cecfb0182d4
Leon Lu <yanglyu.leon.7@gmail.com>
2018-04-20 15:37:00 -0700

    Setup Production Redux Store
    Module 15.2

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-
-export default function configureStore(initialState){
-    return createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(thunk, reduxImmutableStateInvariant())
-    );
+/**
+ * Dynamic imports are NOT supported by ES6,
+ * so one has to use "require" instead of "import".
+ */
+if (process.env.NODE_EVN === 'production') {
+    module.exports = require('./configureStore.prod');
+} else {
+    module.exports = require('./configureStore.dev');
 }
